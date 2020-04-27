b221d5dfbf2bcd87c9f5a22b4c7428581bafc33c
Anand Gangadhara <anand.practice@outlook.com>
2020-04-27 13:48:22 +0100

    Projects added. Web and Test

---
+++ pet-portal-web/pet-portal-web.tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace pet_portal_web.tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
